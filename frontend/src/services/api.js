ff462efa37e8dbf13db22d96dcb119a83a2cdb7a
SHARANYA-216 <sharanyasrinivas216@gmail.com>
2026-08-15 15:52:53 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8001/api';
+const API_BASE_URL = 'https://email-spam-detection-oyhu.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
